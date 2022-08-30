400ed3c66af0e3fe165d9bbad186a415a263b7e1
Fulton <cfulton@luc.edu>
2022-08-30 16:51:14 -0500

    initial commit

---
+++ src/CostCalc.java
@@ -0,0 +1,67 @@
+/*--------------------------------------------
+    Program 2: MPLS Dog Boarding Cost Estimate
+    The MPLS Dog Boarding Company in North Minneapolis is a dog day care facility that is 
+    looking for a developer that will implement functionality that will determine the cost 
+    of boarding fee based on information entered by client. The application will ask for the 
+    name, breed, age, the number of days the dog will need care, and weight of the dog. Once 
+    all information is received, the application will generate a estimate that provides the 
+    estimated cost of stay, the dog's name, age, number or days, weight and boarding group.  
+    The boarding group is based on the dog's age.
+
+    
+	
+	There will be five requirements for Program 1
+	Requirement 1 - Variables are properly declared and initialized; Use of Scanner Object to read 
+                    input from console. Make use of constant final variables. When possible, make 
+                    sure to declare all variables that will hold data
+                    
+	Requirement 2 - Input/Output of all necessary information; Correct calculation including 2% discount.
+                    
+    Requirement 3 - Proper structures used to determine calculated outcome and dog's boarding group; Correct Operators
+    Requirement 4 - Style - Proper use of comments, spacing, in program; use of
+                    descriptive variable names
+    Requirement 5 - Program is submitted by the due date listed and pushed to assigned GitHub Repository; 
+                    Repository contains a minimum of three commits.
+	
+    [REPLACE MY INFORMATION WITH YOURS]
+    Course: COMP 170, Fall 1 2022
+    System: Visual Studio Code, Windows 10
+    Author: C. Fulton
+*/
+
+//IMPORT STATEMENTS
+
+
+public class CostCalc {
+    public static void main(String[] args) throws Exception {
+        //DECLARATIONS 
+
+
+        //INSTANTIATE SCANNER OBJECT
+
+
+
+        //WELCOME MESSAGE 
+        System.out.println("Welcome to MPLS Dog Boarding cost calculator, this application will generate a summary of cost. ");
+
+        //OUTPUT + INPUT OF DOG INFORMATION
+
+
+        //LOGIC TO DETERMINE ESTIMATED COST BASED ON WEIGHT
+
+
+        //DETERMINE ESTIMATED COST BASED ON WEIGHT AND NUMBER OF DAYS
+
+
+        //DETERMINE IF ESTIMATED COST RECEIVE A DISCOUNT
+
+
+        //DETERMINE DOGS BOARDING GROUP BASED ON AGE
+
+
+        //OUTUT SUMMARY OF DOG INFO. AND ESTIMATED COST
+
+
+
+    }
+}
